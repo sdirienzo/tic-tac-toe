8d58f06f3a00a19cbf0d979eb60bd72cd2951e66
Stephen Di Rienzo <stephen.dirienzo@gmail.com>
2021-10-09 19:21:59 -0400

    Introduce logic to create gameboard grid

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,35 @@
 const Player = (name, marker) => {
-    let name = name;
-    let marker = marker;
     const getName = () => name;
     const getMarker = () => marker;
 }
 
 const gameBoard = (() => {
+    const gameboardContainer = document.getElementsByClassName("gameboard-container")[0];
+
     let gameboard = [];
 
     const getGameboard = () => gameboard;
 
-    const clearGameboard = () => {
+    const createGameboard = () => {
+        for (let index = 0; index < 9; index++) {
+            let newDiv = document.createElement("div");
+            newDiv.classList.add("cell");
+            gameboardContainer.appendChild(newDiv);
+        }
+    };
+    
+    const emptyGameboardArray = () => {
         while (gameboard.length > 0) {
             gameboard.pop();
         }
-    }
+    };
+
+    const clearGameboard = () => {
+        while (gameboardContainer.firstChild) {
+            gameboardContainer.removeChild(gameboardContainer.firstChild);
+        }
+        emptyGameboardArray();
+    };
 
     const isPositionFree = position => {
         return !gameboard[position];
@@ -23,4 +38,7 @@ const gameBoard = (() => {
     const addToBoard = (position, marker) => {
         gameboard[position] = marker;
     };
+    return {getGameboard, createGameboard, clearGameboard, isPositionFree, addToBoard}
 })();
+
+gameBoard.createGameboard();
