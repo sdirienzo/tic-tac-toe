2e93685f5f8614bad253aa35043ad89d6b45aafb
Stephen Di Rienzo <stephen.dirienzo@gmail.com>
2021-10-16 11:17:10 -0400

    Introduce displayController module

--- scripts/script.js
+++ scripts/script.js
@@ -8,13 +8,13 @@ const gameBoard = (() => {
 
     const getGameboard = () => gameboard;
 
-    const createGameboard = (gameboardContainer, nodeClass) => {
-        for (let index = 0; index < 9; index++) {
-            let newDiv = document.createElement("div");
-            newDiv.classList.add(`${nodeClass}`);
-            gameboardContainer.appendChild(newDiv);
-        }
-    };
+    // const createGameboard = (gameboardContainer, nodeClass) => {
+    //     for (let index = 0; index < 9; index++) {
+    //         let newDiv = document.createElement("div");
+    //         newDiv.classList.add(`${nodeClass}`);
+    //         gameboardContainer.appendChild(newDiv);
+    //     }
+    // };
     
     const emptyGameboard = () => {
         while (gameboard.length > 0) {
@@ -22,14 +22,14 @@ const gameBoard = (() => {
         }
     };
 
-    const clearGameboard = (gameboardContainer) => {
-        while (gameboardContainer.firstChild) {
-            gameboardContainer.removeChild(gameboardContainer.firstChild);
-        }
-        emptyGameboard();
-    };
+    // const clearGameboard = (gameboardContainer) => {
+    //     while (gameboardContainer.firstChild) {
+    //         gameboardContainer.removeChild(gameboardContainer.firstChild);
+    //     }
+    //     emptyGameboard();
+    // };
 
-    const isPositionFree = position => {
+    const isPositionFree = (position) => {
         return !gameboard[position];
     };
 
@@ -37,11 +37,38 @@ const gameBoard = (() => {
         gameboard[position] = marker;
     };
 
-    const addMarkerToGameboard = (node, position, marker) => {
-        node.innerText = marker;
-        addToGameboard(position, marker);
+    // const addMarkerToGameboard = (node, position, marker) => {
+    //     node.innerText = marker;
+    //     addToGameboard(position, marker);
+    // };
+    return {getGameboard, emptyGameboard, isPositionFree, addToGameboard}
+})();
+
+const displayController = (() => {
+    const gameboardContainer = document.getElementsByClassName("gameboard-container")[0];
+    const startRestartBtn = document.getElementById("start-restart-btn");
+
+    const addMarkerToGameboard = (index, marker) => {
+        gameboardContainer[index].innerText = marker;
     };
-    return {getGameboard, createGameboard, clearGameboard, isPositionFree, addMarkerToGameboard}
+
+    //TODO: add callback function
+    const createGameboard = () => {
+        for (let index = 0; index < 9; index++) {
+            let newDiv = document.createElement("div");
+            newDiv.classList.add("cell");
+            newDiv.addEventListener("click", )
+            gameboardContainer.appendChild(newDiv);
+        }
+    };
+
+    const clearGameboard = () => {
+        while (gameboardContainer.firstChild) {
+            gameboardContainer.removeChild(gameboardContainer.firstChild);
+        }
+    };
+    
+
 })();
 
 // gameBoard.createGameboard();
